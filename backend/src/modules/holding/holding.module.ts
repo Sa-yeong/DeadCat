1488268f2ba5f02baadd1e0ad22ac7152c82d6d2
anyeasulhea <anyeasulhea@naver.com>
2026-06-17 21:18:54 +0900

    holding service수정

--- backend/src/modules/holding/holding.module.ts
+++ backend/src/modules/holding/holding.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { HoldingController } from './holding.controller';
 import { HoldingService } from './holding.service';
 import { HoldingRepository } from './holding.repository';
+import { PriceModule } from '../../price/price.module';
 
 @Module({
+    imports: [PriceModule],
     controllers: [HoldingController],
     providers: [HoldingService, HoldingRepository],
     exports: [HoldingService],
