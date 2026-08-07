5edbddc97282197ccee22bb208f618afe10347f7
seoyoung <baeseoyoung8354@gmail.com>
2026-08-07 18:40:56 +0900

    commit for switching branch

--- backend/src/modules/like-post/like-post.repository.ts
+++ backend/src/modules/like-post/like-post.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/providers/database/prisma.service'; // 프로젝트 내 프리즈마 서비스 경로 확인 필요
+import { PrismaService } from 'src/providers/database/prisma.service';
 
 @Injectable()
 export class LikePostRepository {
@@ -13,22 +13,21 @@ export class LikePostRepository {
             include: {
                 posts: {
                     include: {
-                        users: {
+                        users_posts_writer_idTousers: {
                             select: {
                                 nickname: true,
                             },
                         },
                         _count: {
                             select: {
-                                comments: true, // commentsRelation 수 카운트
-                                like_posts: true, // like_postsRelation 수 카운트
+                                comments: true,
+                                like_posts: true,
                             },
                         },
                     },
                 },
             },
             orderBy: {
-                // 특정 필드가 없다면 릴레이션 순서 혹은 복합키 정렬 처리 가능
                 post_id: 'desc',
             },
         });
